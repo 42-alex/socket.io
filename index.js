118476b161e13ea4610d9f070784ec4a87c628f5
Ironfist <ironfistse@gmail.com>
2023-08-01 15:25:05 -0500

    Added mongoDb for data persistence (as we save it to real database instead keeping it just in memory)

--- index.js
+++ index.js
@@ -1,33 +1,58 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-const messages = [
-    { name: 'Tim', message: 'Hi' },
-    { name: 'Jane', message: 'Hello' },
-]
-
-app.get('/messages', (req, res) => {
-    res.send(messages)
+const Messages = mongoose.model('Messages', {
+    name: String,
+    message: String,
 })
 
-app.post('/messages', (req, res) => {
-    const newMessage = req.body
-    messages.push(newMessage)
-    io.emit('message', req.body),
-    res
-        .status(200)
-        .json(newMessage)
+app.get('/messages', async (req, res) => {
+    try {
+        const messages = await Messages.find({})
+        res.status(200).json(messages)
+    } catch (err) {
+        res.status(500).json({ success: false, message: err })
+    }
 })
 
-io.on('connection', socket => console.log('a user connected '))
- 
+app.post('/messages', async (req, res) => {
+    const message = new Messages(req.body)
+    try {
+        const savedMessage = await message.save()
+        io.emit('message', savedMessage)
+        res.status(201).json(savedMessage)
+    } catch (err) {
+        res.status(500).json({ success: false, message: err })
+    }
+})
+
+io.on('connection', socket => {
+    console.log('a user connected ', socket?.client?.conn?.id)
+});
+
+
+const dbUrl = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@mongoose.o02dyue.mongodb.net/?retryWrites=true&w=majority`;
+(async function mongooseConnect() {
+    try {
+        await mongoose.connect(dbUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connected to MongoDB successfully!')
+    } catch (err) {
+        console.error('MongoDB connection error: ', err)
+    }
+})()
+
 const server = http.listen(
-    3000, 
-    () => console.log('server is running on port: ', server.address().port),
+  3000,
+  () => console.log('server is running on port: ', server.address().port),
 )
